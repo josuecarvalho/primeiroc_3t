cdd06d5333a1de4bbbfa0f79c810a822efba0dae
josuecarvalho <110104813+josuecarvalho@users.noreply.github.com>
2022-10-19 10:02:54 -0300

    prova das somas

--- estudo.js
+++ estudo.js
@@ -28,3 +28,24 @@ function cubo(){
         document.write("O cubo de "+i+" é "+i+" x "+i+" x "+i+" = "+(i*i*i)+"<br>");
     }
 }
+function media(){
+    let n1 =document.getElementById("v1").value;
+    let n2 =document.getElementById("v2").value;
+    let n3 =document.getElementById("v3").value;
+    let r =(Number(n1) + Number(n2) + Number(n3))/3;
+    document.getElementById("resultado").innerHTML = "Média: " + r;
+}
+function somar(){
+    let n4 =document.getElementById("v4").value;
+    let n5 =document.getElementById("v5").value;
+    let n6 =document.getElementById("v6").value;
+    let r =(Number(n4) + Number(n5) + Number(n6));
+    document.getElementById("resultado da Soma").innerHTML = "Soma: " + r;
+}
+function subtrair(){
+    let n7 =document.getElementById("v7").value;
+    let n8 =document.getElementById("v8").value;
+    let n9 =document.getElementById("v9").value;
+    let r =(Number(n7) - Number(n8) - Number(n9));
+    document.getElementById("resultado da Subtração").innerHTML = "Subtração: " + r;
+}
