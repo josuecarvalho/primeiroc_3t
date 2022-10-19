7f3450456d68f41d290aabcb45e034e1295df30f
josuecarvalho <110104813+josuecarvalho@users.noreply.github.com>
2022-10-19 09:24:19 -0300

    Add files via upload

--- estudo.js
+++ estudo.js
@@ -1,4 +1,4 @@
-let tabuada = 2;
+let tabuada = 5;
 
 function escreva(){
     document.write(tabuada + " x 1 = " + (tabuada*1) + "<br>");
@@ -11,4 +11,20 @@ function escreva(){
     document.write(tabuada + " x 8 = " + (tabuada*8) + "<br>");
     document.write(tabuada + " x 9 = " + (tabuada*9) + "<br>");
     document.write(tabuada + " x 10 = " + (tabuada*10) + "<br>");
+}
+
+function minhaTabuada(){
+    for(let i = 10; i >= 1; i--){
+        document.write("Tabuada do " + i + "<br>");
+        for(let j = 1; j <= 10; j++){
+            document.write(i + " x " + j + " = " + (j*i) + "<br>");
+        }
+        document.write("<br>");       
+    }
+}
+
+function cubo(){
+    for(let i = 20; i >= 2; i--){
+        document.write("O cubo de "+i+" é "+i+" x "+i+" x "+i+" = "+(i*i*i)+"<br>");
+    }
 }
